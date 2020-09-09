04d76daacbf7888b62010032044d6f1cf918cb3a
sitetester <sitetester2010@gmail.com>
2020-09-09 16:31:11 +0300

    eslint applied

--- src/repository/BlocksRepository.ts
+++ src/repository/BlocksRepository.ts
@@ -1,23 +1,23 @@
-import EthBlock from '../entities/EthBlock';
-import dao from '../../db/dao';
+import EthBlock from '../entities/EthBlock'
+import dao from '../../db/dao'
 
 export default class BlocksRepository {
     readonly BLOCKS_TABLE = 'blocks';
 
-    async getLastScannedBlock() {
-        const lastScannedBlockNumber = await dao
-            .orderBy('blockNumber', 'desc')
-            .from(this.BLOCKS_TABLE)
-            .first();
+    async getLastScannedBlock () {
+      const lastScannedBlockNumber = await dao
+        .orderBy('blockNumber', 'desc')
+        .from(this.BLOCKS_TABLE)
+        .first()
 
-        if (lastScannedBlockNumber === undefined) {
-            return 0;
-        }
+      if (lastScannedBlockNumber === undefined) {
+        return 0
+      }
 
-        return lastScannedBlockNumber.blockNumber;
+      return lastScannedBlockNumber.blockNumber
     }
 
-    async persistBlocks(blocks: EthBlock[]) {
-        await dao(this.BLOCKS_TABLE).insert(blocks);
+    async persistBlocks (blocks: EthBlock[]) {
+      await dao(this.BLOCKS_TABLE).insert(blocks)
     }
 }
